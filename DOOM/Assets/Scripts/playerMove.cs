03c210e5e32cb2b5752da31b6f03f2570f9d2374
Allan Felipe Malinoski <allanfelipedk@gmail.com>
2021-06-19 16:39:15 -0300

    Weapon

--- DOOM/Assets/Scripts/playerMove.cs
+++ DOOM/Assets/Scripts/playerMove.cs
@@ -4,49 +4,31 @@ using UnityEngine;
 
 public class playerMove : MonoBehaviour
 {
+    public float speed;
+
+    Rigidbody rb;
+
     private float yaw = 0.0f;
-    private float pith = 0.0f;
+    private float pitch = 0.0f;
 
-    public float spd = 0.02f;
-
-    private float speedH = 2.0f;
-    private float speedV = 2.0f;
+    public float moveCamera;
 
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody>();
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        if(Input.GetKey("w"))
-        {
-            transform.position += (transform.forward * spd);
-        }
-        else if(Input.GetKey("s"))
-        {
-            transform.position -= (transform.forward * spd);
-        }
+        Vector3 Position = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        rb.velocity = Position * speed;
 
-        if(Input.GetKey("d"))
-        {
-            float dir = transform.eulerAngles.y - 90.0f;
-            Vector3 spdTemp = new Vector3(Mathf.Sign(dir * Mathf.Deg2Rad),0,Mathf.Cos(dir * Mathf.Deg2Rad));
-            transform.position = transform.position + (spdTemp * -spd);
-        }
-        else if(Input.GetKey("a"))
-        {
-            float dir = transform.eulerAngles.y + 90.0f;
-            Vector3 spdTemp = new Vector3(Mathf.Sign(dir * Mathf.Deg2Rad), 0, Mathf.Cos(dir * Mathf.Deg2Rad));
-            transform.position = transform.position + (spdTemp * -spd);
-        }
-
-        yaw += speedH * Input.GetAxis("Mouse X");
-        pith -= speedV * Input.GetAxis("Mouse Y");
-        transform.eulerAngles = new Vector3(pith,yaw, 0.0f);
-
+        yaw += moveCamera * Input.GetAxis("Mouse X");
+        pitch -= moveCamera * Input.GetAxis("Mouse Y");
+        transform.eulerAngles = new Vector3(pitch,yaw,0.0f);
     }
 }
